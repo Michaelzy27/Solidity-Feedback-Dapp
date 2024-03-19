06cb3dcb51a57d6026c86f02bd4e8bd5e7fdb1e4
Michaelzy27 <umehmichael27@gmail.com>
2024-03-19 22:44:01 +0100

    completing task

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -17,7 +17,7 @@ contract HelloWorld {
         return message;
     }
 
-    function giveFeedback(string memory hi) public {
+    function addFeedback(string memory hi) public {
         //Feedbacks.
         feed.push(hi);
 
--- scripts/run.js
+++ scripts/run.js
@@ -6,14 +6,53 @@ const main = async () => {
     const message = await helloContract.getMessage();
     console.log(message);
 
-    await helloContract.giveFeedback("myy");
+    await helloContract.addFeedback("myy");
 
     const feedbacks = await helloContract.getFeedbacks()
     console.log(feedbacks);
 
+    
+
+
+
 
 
 };
+
+const addFeedbackButton = document.getElementById("add");
+addFeedbackButton.addEventListener("click", () => {addFeedback();});  //when submit button in html is clicked
+
+function addFeedback() {
+    const newFeedback = document.getElementById("thefeedback").value;      //gets the feedback from the text field
+    //helloContract.addFeedback(newFeedback);
+    document.getElementById("thefeedback").value = "hhh";
+}
+
+const connectWalletButton = document.getElementById("connect");
+  connectWalletButton.addEventListener("click", () => {
+    connectWallet();
+  });
+
+function connectWallet() {
+    if (typeof window.ethereum !== "undefined") {
+      ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((accounts) => {
+          currentAccount = accounts[0];
+          document.getElementById("account").textContent = currentAccount;
+          initializeContract();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      alert("Please install MetaMask to use this DApp.");
+    }
+  }
+
+  
+
+
 
 const runMain = async () => {
     try {
