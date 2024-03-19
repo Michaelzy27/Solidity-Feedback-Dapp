2157c359118cddc2c11b19a0a70960ca84c9d14c
Michaelzy27 <umehmichael27@gmail.com>
2024-03-19 18:25:03 +0100

    first solidty dapp

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.24;
+
+import "hardhat/console.sol";
+
+contract HelloWorld {
+    constructor(){
+        console.log("this is a smart contract");
+    }
+
+    string message = "Hello Michael";
+    uint[] public Feedbacks = [1, 2];
+    string[] feed = ["hi", "hii"];
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+
+    function giveFeedback(string hi) public view {
+        //Feedbacks.
+        feed.push(hi);
+
+    }
+
+    function getFeedbacks() public view returns(string[] memory) {
+        return feed;
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,26 @@
+const main = async () => {
+    const helloContractFactory = await hre.ethers.getContractFactory("HelloWorld");
+    const helloContract = await helloContractFactory.deploy();
+    //await helloContract.deployed();
+
+    const message = await helloContract.getMessage();
+    console.log(message);
+
+    const feedbacks = await helloContract.getFeedbacks()
+    console.log(feedbacks);
+
+
+
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);   // exit node process without error
+    } catch (error) {
+        console.log(error);
+        process.exit(1);   // exit node process while indicating 'Uncaught Fatal Exception' error
+    }
+}
+
+runMain();
